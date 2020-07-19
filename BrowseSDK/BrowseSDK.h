6e37c8e1c97fc2dc11e63d67c7dcec45bfb58619
James Lawton <jlawton@box.com>
2020-07-18 20:49:31 -0700

    Configure linting

--- BrowseSDK/BrowseSDK.h
+++
@@ -1,8 +0,0 @@
-//
-//  Created on 7/18/20.
-//  Copyright © 2020 Box. All rights reserved.
-//
-
-@import Foundation;
-
-// In this header, you should import all the public headers of your framework using statements like #import <BrowseSDK/PublicHeader.h>
---
+++ BrowseSDK/View/BrowseViewController.swift
@@ -0,0 +1,8 @@
+//
+//  Created on 7/18/20.
+//  Copyright © 2020 Box. All rights reserved.
+//
+
+import UIKit
+
+public class BrowseViewController: UIViewController {}
