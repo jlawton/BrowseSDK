32f554469e7121d6bbb8156536755f2a770ea5e4
James Lawton <jlawton@box.com>
2020-07-20 03:00:38 -0700

    Add sample app

---
+++ BrowseSampleApp/BrowseSampleApp/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  Created on 7/20/20.
+//  Copyright © 2020 Box. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
