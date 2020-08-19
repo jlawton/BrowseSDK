5dc47a75b4f58e53b56d1ec0070ff59d7b9a6c03
James Lawton <jlawton@box.com>
2020-08-19 02:34:47 -0700

    Refactor MenuButton to support creating UIButton

--- BrowseSDK/View/iOS13/MenuButton.swift
+++ BrowseSDK/View/iOS13/MenuButton.swift
@@ -7,11 +7,21 @@ import ObjectiveC
 import UIKit
 
 class MenuButton: NSObject, UIContextMenuInteractionDelegate {
-    let systemItem: UIBarButtonItem.SystemItem
+    enum Style {
+        case systemItem(UIBarButtonItem.SystemItem)
+        case image(UIImage, alt: String)
+    }
+
+    let style: Style
     let menu: UIMenu
 
     init(systemItem: UIBarButtonItem.SystemItem, menu: UIMenu) {
-        self.systemItem = systemItem
+        style = .systemItem(systemItem)
+        self.menu = menu
+    }
+
+    init(image: UIImage, alt: String, menu: UIMenu) {
+        style = .image(image, alt: alt)
         self.menu = menu
     }
 
@@ -21,9 +31,9 @@ class MenuButton: NSObject, UIContextMenuInteractionDelegate {
 
     func barButtonItem() -> UIBarButtonItem {
         if #available(iOSApplicationExtension 14.0, *) {
-            return createButton14()
+            return createBarButtonItem14()
         }
-        let item = createButton13()
+        let item = createBarButtonItem13()
         objc_setAssociatedObject(
             item, &Self.MenuButtonKey,
             self, .OBJC_ASSOCIATION_RETAIN_NONATOMIC
@@ -31,38 +41,62 @@ class MenuButton: NSObject, UIContextMenuInteractionDelegate {
         return item
     }
 
+    func button() -> UIButton {
+        if #available(iOSApplicationExtension 14.0, *) {
+            return createButton14()
+        }
+        let button = createButton13()
+        objc_setAssociatedObject(
+            button, &Self.MenuButtonKey,
+            self, .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+        )
+        return button
+    }
+
     @available(iOS 14.0, *)
-    private func createButton14() -> UIBarButtonItem {
-        UIBarButtonItem(systemItem: systemItem, menu: menu)
+    private func createBarButtonItem14() -> UIBarButtonItem {
+        switch style {
+        case let .systemItem(systemItem):
+            return UIBarButtonItem(systemItem: systemItem, menu: menu)
+        case let .image(image, alt: alt):
+            let button = UIBarButtonItem(image: image, menu: menu)
+            button.accessibilityLabel = alt
+            return button
+        }
+    }
+
+    @available(iOS 14.0, *)
+    private func createButton14() -> UIButton {
+        let image = style.asImage
+        let button = UIButton.systemButton(
+            with: image.image,
+            target: nil, action: nil
+        )
+        button.accessibilityLabel = image.alt
+        button.menu = menu
+        button.showsMenuAsPrimaryAction = true
+        return button
     }
 
     @available(iOS, deprecated: 14.0)
-    private func createButton13() -> UIBarButtonItem {
+    private func createBarButtonItem13() -> UIBarButtonItem {
         // Use a custom view for the button so we can add a context menu
         // interaction triggered by long-press on iOS 13. A tap will instead
         // trigger the selector.
-        let button: UIButton
-        // swiftlint:disable force_unwrapping
-        switch systemItem {
-        case .add:
-            button = UIButton.systemButton(
-                with: UIImage(systemName: "plus")!,
-                target: self, action: #selector(buttonTapped(_:))
-            )
-            button.accessibilityLabel = "Add"
-        case .action:
-            button = UIButton.systemButton(
-                with: UIImage(systemName: "square.and.arrow.up")!,
-                target: self, action: #selector(buttonTapped(_:))
-            )
-            button.accessibilityLabel = "Share"
-        default:
-            preconditionFailure("Unsupported button type")
-        }
-        // swiftlint:enable force_unwrapping
-
-        button.addInteraction(UIContextMenuInteraction(delegate: self))
+        let button = createButton13()
         return UIBarButtonItem(customView: button)
+    }
+
+    @available(iOS, deprecated: 14.0)
+    private func createButton13() -> UIButton {
+        let image = style.asImage
+        let button = UIButton.systemButton(
+            with: image.image,
+            target: self, action: #selector(buttonTapped(_:))
+        )
+        button.accessibilityLabel = image.alt
+        button.addInteraction(UIContextMenuInteraction(delegate: self))
+        return button
     }
 
     // Used to render the menu from the add button's context menu interaction
@@ -87,5 +121,29 @@ class MenuButton: NSObject, UIContextMenuInteractionDelegate {
         if let controller = responder as? UIViewController {
             controller.present(alert, animated: true, completion: nil)
         }
+    }
+}
+
+extension MenuButton.Style {
+    var asImage: (image: UIImage, alt: String) {
+        switch self {
+        case let .systemItem(systemItem):
+            return imageForSystemItem(systemItem)
+        case let .image(image, alt):
+            return (image, alt)
+        }
+    }
+
+    func imageForSystemItem(_ systemItem: UIBarButtonItem.SystemItem) -> (image: UIImage, alt: String) {
+        // swiftlint:disable force_unwrapping
+        switch systemItem {
+        case .add:
+            return (UIImage(systemName: "plus")!, "Add")
+        case .action:
+            return (UIImage(systemName: "square.and.arrow.up")!, "Share")
+        default:
+            preconditionFailure("Unsupported button type")
+        }
+        // swiftlint:enable force_unwrapping
     }
 }
